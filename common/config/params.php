91dadb5464fc71a20949be73a3d7938ae712b378
njugunamwangi <njugunamwangi.n@gmail.com>
2023-04-19 14:20:42 +0300

    init

---
+++ common/config/params.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'adminEmail' => 'admin@in2dronestechsolutions.com',
+    'supportEmail' => 'info@in2dronestechsolutions.com',
+    'senderEmail' => 'info@in2dronestechsolutions.com',
+    'senderName' => 'in2 Drones Store',
+    'user.passwordResetTokenExpire' => 3600,
+    'user.passwordMinLength' => 8
+];
